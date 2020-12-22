f69849710871f74706568fd0eca489cd29573508
blitzdawolf <botjes2@hotmail.com>
2020-12-22 20:27:53 +0100

    Added items to unitOfWork

--- Tennis.BLL/UnitOfWork.cs
+++ Tennis.BLL/UnitOfWork.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using Tennis.DAL.Context;
 using Tennis.DAL.Repository;
+using Tennis.DAL.Repository.Interface;
 
 namespace Tennis.BLL
 {
@@ -18,8 +19,22 @@ namespace Tennis.BLL
             this.mapper = mapper;
         }
 
-        private RoleRepository roleRepository;
+        private IFineRepository fineRepository;
+        private IGameRepository gameRepository;
+        private IGameResultRepository gameResultRepository;
+        private IGenderRepository genderRepository;
+        private ILeagueRepository leagueRepository;
+        private IMemberRepository memberRepository;
+        private IMemberRoleRepository memberRoleRepository;
+        private IRoleRepository roleRepository;
 
-        public RoleRepository RoleRepository => this.roleRepository ??= new RoleRepository(context, mapper);
+        public IFineRepository FineRepository => this.fineRepository ??= new FineRepository(context, mapper);
+        public IGameRepository GameRepository => this.gameRepository ??= new GameRepository(context, mapper);
+        public IGameResultRepository GameResultRepository => this.gameResultRepository ??= new GameResultRepository(context, mapper);
+        public IGenderRepository GenderRepository => this.genderRepository ??= new GenderRepository(context, mapper);
+        public ILeagueRepository LeagueRepository => this.leagueRepository ??= new LeagueRepository(context, mapper);
+        public IMemberRepository MemberRepository => this.memberRepository ??= new MemberRepository(context, mapper);
+        public IMemberRoleRepository MemberRoleRepository => this.memberRoleRepository ??= new MemberRoleRepository(context, mapper);
+        public IRoleRepository RoleRepository => this.roleRepository ??= new RoleRepository(context, mapper);
     }
 }
